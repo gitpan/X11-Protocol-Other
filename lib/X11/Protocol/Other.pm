b99a3c2f2d9792a956c79975855da2a54a0d1c0c
Kevin Ryde <kryde@cpan.org>
2011-03-01 21:20:21 +0000

    Import of KRYDE/X11-Protocol-Other-1 from CPAN.
    gitpan-cpan-distribution: X11-Protocol-Other
    gitpan-cpan-version:      1
    gitpan-cpan-path:         KRYDE/X11-Protocol-Other-1.tar.gz
    gitpan-cpan-author:       KRYDE
    gitpan-cpan-maturity:     released

---
+++ lib/X11/Protocol/Other.pm
@@ -0,0 +1,262 @@
+# Copyright 2010, 2011 Kevin Ryde
+
+# This file is part of X11-Protocol-Other.
+#
+# X11-Protocol-Other is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3, or (at your option) any later
+# version.
+#
+# X11-Protocol-Other is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with X11-Protocol-Other.  If not, see <http://www.gnu.org/licenses/>.
+
+## no critic (RequireUseWarnings)
+package X11::Protocol::Other;
+use 5.004;
+use strict;
+use Carp;
+
+use vars '$VERSION', '@ISA', '@EXPORT_OK';
+$VERSION = 1;
+
+use Exporter;
+@ISA = ('Exporter');
+@EXPORT_OK = qw(root_to_screen
+                root_to_screen_info
+                default_colormap_to_screen
+                default_colormap_to_screen_info
+                visual_is_dynamic
+                visual_class_is_dynamic
+                window_size
+                window_visual);
+
+# uncomment this to run the ### lines
+#use Smart::Comments;
+
+sub window_size {
+  my ($X, $window) = @_;
+  ### Other window_size(): "$X $window"
+  if (my $screen = root_to_screen_info($X,$window)) {
+    return ($screen->{'width_in_pixels'}, $screen->{'height_in_pixels'});
+  }
+  my %geom = $X->GetGeometry ($window);
+  return ($geom{'width'}, $geom{'height'});
+}
+sub window_visual {
+  my ($X, $window) = @_;
+  ### Other window_visual(): "$X $window"
+  if (my $screen = root_to_screen_info($X,$window)) {
+    return $screen->{'root_visual'};
+  }
+  my %attr = $X->GetWindowAttributes ($window);
+  return $attr{'visual'};
+}
+
+#------------------------------------------------------------------------------
+
+sub root_to_screen {
+  my ($X, $root) = @_;
+  ### Other root_to_screen(): $root
+  return ($X->{__PACKAGE__.'.root_to_screen_number'}
+          ||= { map {($X->{'screens'}->[$_]->{'root'} => $_)}
+                0 .. $#{$X->{'screens'}} })
+    ->{$root};
+}
+sub root_to_screen_info {
+  my ($X, $root) = @_;
+  ### Other root_to_screen_info(): $root
+  my $ret;
+  if (defined ($ret = root_to_screen($X,$root))) {
+    $ret = $X->{'screens'}->[$ret];
+  }
+  return $ret;
+
+  # return ($X->{__PACKAGE__.'.root_to_screen_info'}
+  #         ||= { map {($_->{'root'} => $_)} @{$X->{'screens'}} })->{$root}
+}
+
+#------------------------------------------------------------------------------
+
+sub default_colormap_to_screen {
+  my ($X, $colormap) = @_;
+  ### default_colormap_to_screen(): $colormap
+  return ($X->{__PACKAGE__.'.default_colormap_to_screen_number'}
+          ||= { map {($X->{'screens'}->[$_]->{'default_colormap'} => $_)}
+                0 .. $#{$X->{'screens'}} })
+    ->{$colormap};
+}
+sub default_colormap_to_screen_info {
+  my ($X, $colormap) = @_;
+  ### Other colormap_to_screen_info(): $colormap
+  my $ret;
+  if (defined ($ret = default_colormap_to_screen($X,$colormap))) {
+    $ret = $X->{'screens'}->[$ret];
+  }
+  return $ret;
+}
+
+# # return true if $colormap is one of the screen default colormaps
+# sub colormap_is_default {
+#   my ($X, $colormap) = @_;
+#   return defined (default_colormap_to_screen($X,$colormap));
+# }
+
+
+#------------------------------------------------------------------------------
+# my %visual_class_is_dynamic = (StaticGray  => 0,  0 => 0,
+#                                GrayScale   => 1,  1 => 1,
+#                                StaticColor => 0,  2 => 0,
+#                                PseudoColor => 1,  3 => 1,
+#                                TrueColor   => 0,  4 => 0,
+#                                DirectColor => 1,  5 => 1,
+#                               );
+sub visual_class_is_dynamic {
+  my ($X, $visual_class) = @_;
+  return $X->num('VisualClass',$visual_class) & 1;
+}
+sub visual_is_dynamic {
+  my ($X, $visual_id) = @_;
+  my $visual_info = $X->{'visuals'}->{$visual_id}
+    || croak 'Unknown visual ',$visual_id;
+  return visual_class_is_dynamic ($X, $visual_info->{'class'});
+}
+
+
+#------------------------------------------------------------------------------
+
+# # return true if $pixel is black or white in the default root window colormap
+# sub pixel_is_black_or_white {
+#   my ($X, $pixel) = @_;
+#   return ($pixel == $X->{'black_pixel'} || $pixel == $X->{'white_pixel'});
+# }
+# 
+
+1;
+__END__
+
+=for stopwords Ryde XID colormap colormaps ie PseudoColor
+
+=head1 NAME
+
+X11::Protocol::Other -- miscellaneous X11::Protocol helpers
+
+=head1 SYNOPSIS
+
+ use X11::Protocol::Other;
+
+=head1 DESCRIPTION
+
+This is some helper functions for C<X11::Protocol>.
+
+=head1 EXPORTS
+
+Nothing is exported by default, but the functions can be requested in usual
+C<Exporter> style,
+
+    use X11::Protocol::Other 'visual_is_dynamic';
+    if (visual_is_dynamic ($X, $visual_id)) {
+      ...
+    }
+
+Or just called with full package name
+
+    use X11::Protocol::Other;
+    if (X11::Protocol::Other::visual_is_dynamic ($X, $visual_id)) {
+      ...
+    }
+
+There's no C<:all> tag since this module is meant as a grab-bag of functions
+and to import as-yet unknown things would be asking for name clashes.
+
+=head1 FUNCTIONS
+
+=head2 Screen Finding
+
+=over 4
+
+=item C<<  $number = root_to_screen ($X, $root) >>
+
+=item C<<  $hashref = root_to_screen_info ($X, $root) >>
+
+Return the screen number or screen info hash for a given root window.
+C<$root> can be any XID integer on C<$X>.  If it's not one of the root
+windows then the return is C<undef>.
+
+=item C<<  $number = default_colormap_to_screen ($X, $colormap) >>
+
+=item C<<  $hashref = default_colormap_to_screen_info ($X, $colormap) >>
+
+Return the screen number or screen info hash for a given default colormap.
+C<$colormap> can be any XID integer on C<$X>.  If it's not one of the screen
+default colormaps then the return is C<undef>.
+
+=back
+
+=head2 Visuals
+
+=over
+
+=item C<<  $bool = visual_is_dynamic ($X, $visual_id) >>
+
+=item C<<  $bool = visual_class_is_dynamic ($X, $visual_class) >>
+
+Return true if the given visual is dynamic, meaning colormap entries on it
+can be changed to change the colour of a given pixel value.
+
+C<$visual_id> is one of the visual ID numbers, ie. one of the keys in
+C<$X-E<gt>{'visuals'}>.  Or C<$visual_class> is a VisualClass string like
+"PseudoColor" or corresponding integer such as 3.
+
+=back
+
+=head2 Window Info
+
+=over
+
+=item C<<  ($width, $height) = window_size ($X, $window) >>
+
+=item C<<  $visual_id = window_visual ($X, $window) >>
+
+Return the size or visual ID of a given window.
+
+C<$window> is an integer XID on C<$X>.  If it's one of the root windows then
+the return values are from the screen info hash in C<$X>, otherwise the
+server is queried with C<GetGeometry> or C<GetWindowAttributes>.
+
+These functions are handy when there's a good chance C<$window> might be a
+root window and therefore not need a server round trip.
+
+=back
+
+=head1 SEE ALSO
+
+L<X11::Protocol>,
+L<X11::Protocol::GrabServer>
+
+=head1 HOME PAGE
+
+http://user42.tuxfamily.org/x11-protocol-other/index.html
+
+=head1 LICENSE
+
+Copyright 2010, 2011 Kevin Ryde
+
+X11-Protocol-Other is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 3, or (at your option) any later
+version.
+
+X11-Protocol-Other is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+more details.
+
+You should have received a copy of the GNU General Public License along with
+X11-Protocol-Other.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
---
+++ t/Other.t
@@ -0,0 +1,148 @@
+#!/usr/bin/perl -w
+
+# Copyright 2011 Kevin Ryde
+
+# This file is part of X11-Protocol-Other.
+#
+# X11-Protocol-Other is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 3, or (at your option) any later
+# version.
+#
+# X11-Protocol-Other is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with X11-Protocol-Other.  If not, see <http://www.gnu.org/licenses/>.
+
+BEGIN { require 5 }
+use strict;
+use Test;
+
+use lib 't';
+use MyTestHelpers;
+BEGIN { MyTestHelpers::nowarnings() }
+
+my $test_count = 10;
+plan tests => $test_count;
+
+require X11::Protocol::Other;
+
+require X11::Protocol;
+MyTestHelpers::diag ("X11::Protocol version ", X11::Protocol->VERSION);
+
+my $display = $ENV{'DISPLAY'};
+if (! defined $display) {
+  MyTestHelpers::diag ('No DISPLAY set');
+  foreach (1 .. $test_count) {
+    skip ('No DISPLAY set', 1, 1);
+  }
+  exit 0;
+}
+MyTestHelpers::diag ("DISPLAY $display");
+
+# pass display arg so as not to get a "guess" warning
+my $X;
+if (! eval { $X = X11::Protocol->new ($display); }) {
+  MyTestHelpers::diag ("Cannot connect to X server -- $@");
+  foreach (1 .. $test_count) {
+    skip ("Cannot connect to X server", 1, 1);
+  }
+  exit 0;
+}
+MyTestHelpers::X11_server_info($X);
+
+$X->QueryPointer($X->{'root'});  # sync
+
+#------------------------------------------------------------------------------
+# VERSION
+
+my $want_version = 1;
+ok ($X11::Protocol::Other::VERSION,
+    $want_version,
+    'VERSION variable');
+ok (X11::Protocol::Other->VERSION,
+    $want_version,
+    'VERSION class method');
+
+ok (eval { X11::Protocol::Other->VERSION($want_version); 1 },
+    1,
+    "VERSION class check $want_version");
+my $check_version = $want_version + 1000;
+ok (! eval { X11::Protocol::Other->VERSION($check_version); 1 },
+    1,
+    "VERSION class check $check_version");
+
+#------------------------------------------------------------------------------
+# root_to_screen()
+
+{
+  ## no critic (ProtectPrivateSubs)
+  my $screens_aref = $X->{'screens'};
+  my $good = 1;
+  foreach my $screen_number (0 .. $#$screens_aref) {
+    my $rootwin = $screens_aref->[$screen_number]->{'root'}
+      || die "oops, no 'root' under screen $screen_number";
+    my $got = X11::Protocol::Other::root_to_screen($X,$rootwin);
+    if (! defined $got || $got != $screen_number) {
+      $good = 0;
+      MyTestHelpers::diag ("root_to_screen() wrong on rootwin $rootwin screen $screen_number");
+      MyTestHelpers::diag ("got ", (defined $got ? $got : 'undef'));
+    }
+  }
+  ok ($good, 1, "root_to_screen()");
+}
+
+#------------------------------------------------------------------------------
+# visual_class_is_dynamic()
+
+{
+  my $visual_class = 'PseudoColor';
+  ok (X11::Protocol::Other::visual_class_is_dynamic($X,$visual_class),
+      1,
+      "visual_class_is_dynamic() $visual_class");
+}
+{
+  my $visual_class = 3;
+  ok (X11::Protocol::Other::visual_class_is_dynamic($X,$visual_class),
+      1,
+      "visual_class_is_dynamic() $visual_class");
+}
+{
+  my $visual_class = 'TrueColor';
+  ok (X11::Protocol::Other::visual_class_is_dynamic($X,$visual_class),
+      0,
+      "visual_class_is_dynamic() $visual_class");
+}
+{
+  my $visual_class = 4;
+  ok (X11::Protocol::Other::visual_class_is_dynamic($X,$visual_class),
+      0,
+      "visual_class_is_dynamic() $visual_class");
+}
+
+#------------------------------------------------------------------------------
+# visual_is_dynamic()
+
+{
+  my $good = 1;
+  foreach (keys %{$X->{'visuals'}}) {
+    my $visual_id = $_;
+    my $visual_class = $X->{'visuals'}->{$visual_id}->{'class'};
+    my $got = X11::Protocol::Other::visual_is_dynamic($X,$visual_id);
+    my $want = X11::Protocol::Other::visual_class_is_dynamic($X,$visual_class);
+    if ($got != $want) {
+      MyTestHelpers::diag ("wrong: visual_id $visual_id visual_class $visual_class got $got want $want");
+      $good = 0;
+    }
+  }
+  ok ($good, 1,
+     'visual_is_dynamic() ');
+}
+
+#------------------------------------------------------------------------------
+$X->QueryPointer($X->{'root'});  # sync
+
+exit 0;
